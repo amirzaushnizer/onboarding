ab5d3cf7dbfcbcd42178d04f43a2f7582e1893c4
amirzaushnizer <amirzau@wix.com>
2021-11-24 16:15:47 +0200

    finish html & css

---
+++ index.ts
@@ -0,0 +1,36 @@
+interface Post {
+    imgUrl: string,
+    postTitle: string,
+    postContent: string,
+    postDate: Date
+}
+
+const initPosts: Post[] = [
+    {
+        imgUrl: 'photos/neo_grass.JPG',
+        postTitle: 'Trip to the Park',
+        postContent: 'Neo and i are taking an afternoon walk at the park :)',
+        postDate: new Date(2021, 11, 10)
+    },
+    {
+        imgUrl: 'photos/neo_amir_yasmin.JPG',
+        postTitle: 'Visiting the family home',
+        postContent: 'A weekend trip to visit our parents',
+        postDate: new Date(2021, 11, 10)
+    },
+    {
+        imgUrl: 'photos/neo_yasmin.JPG',
+        postTitle: 'Neo the guardian',
+        postContent: 'When Yasmin is laying dead on the couch after a long day, Neo is always there to watch her back!',
+        postDate: new Date(2021, 11, 10)
+    },
+    {
+        imgUrl: 'photos/neo_toilet.JPG',
+        postTitle: 'Where is Neo?',
+        postContent: 'Spot the little dog!',
+        postDate: new Date(2021, 11, 10)
+    },
+]
+
+
+
